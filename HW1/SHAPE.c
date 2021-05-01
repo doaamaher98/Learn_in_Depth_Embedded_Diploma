143e9ceeb96a223e172251aa3d086d5ff655bae8
Your Name <you@example.com>
2021-05-01 08:28:03 +0200

    Home-work1

---
+++ HW1/SHAPE.c
@@ -0,0 +1,20 @@
+/*****************************************************/
+/* Name        : Doaa Mohamed Maher 				 */
+/* Date 	   : 30/5/2020							 */
+/* Version     : 1.0								 */
+/* Unit/Assign : Unit(2) Lesson (3) , Assignment (1) */
+/*****************************************************/
+#include "stdio.h"
+
+/* EX:8 */
+void main()
+{
+	int i,j;
+
+	for(i=0; i<=9; i++) {
+	    for(j=i; j<=9; j++) {
+	      printf("%d ", j);
+	    }
+	    printf( "\n" );
+	  }
+}
